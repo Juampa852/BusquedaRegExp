598ebf232fce768f08a600aff1261eb40d5316b5
Juampa Monroy <juampa2345@gmail.com>
2017-03-18 22:38:48 -0600

    Cambios

--- src/Clases/Buscador.java
+++ src/Clases/Buscador.java
@@ -4,14 +4,62 @@
  * and open the template in the editor.
  */
 package Clases;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-
+import javax.swing.JOptionPane;
+import java.math.*; 
 /**
  *
  * @author Juampa Monroy
  */
 public class Buscador {
+    private String regEx;
+    private String validar;
+    private ArrayList<String> lista=new ArrayList<>();
     
+    private File seleccion;
+    private String nombre, extension;
+    long menor, mayor;
+    public Buscador(File seleccion, String nombre, String extension, double menor, double mayor){
+        this.seleccion=seleccion;
+        this.nombre=nombre;
+        this.extension=extension;
+        this.menor= (long) Math.ceil(menor);
+        this.mayor=(long)Math.ceil(mayor);
+    }
+    private void recorrer(File file) {
+        lista= new ArrayList<>();
+        File archivos[] = file.listFiles();
+        if (archivos != null) {
+            for (int i = 0; i < archivos.length; i++) {
+                if (archivos[i].isDirectory()) {
+                    recorrer(archivos[i]);
+                } else {
+                    lista.add(archivos[i].getAbsolutePath());
+                }
+            }
+        }
+    }
+    public ArrayList<String> validos (File directorio){
+        ArrayList<String> validos= new ArrayList<>();
+        
+        return validos;
+    }
+//    private boolean validar(String expReg, String cadena){
+//        
+//    }
+//    String regex=".*"+busquedaField.getText()+".*";
+//        String busqueda=jTextField1.getText();
+//        Pattern pattern = Pattern.compile(regex);
+//        Matcher matcher = pattern.matcher(busqueda);
+//        //JOptionPane.showMessageDialog(null, matcher.find());
+//        while (matcher.find()) {
+//            System.out.println("Full match: " + matcher.group(0));
+//            for (int i = 1; i <= matcher.groupCount(); i++) {
+//                System.out.println("Group " + i + ": " + matcher.group(i));
+//            }
+//        }
 }
--- src/Clases/Ventana1.java
+++ src/Clases/Ventana1.java
@@ -4,15 +4,20 @@
  * and open the template in the editor.
  */
 package Clases;
+import java.io.File;
+import static java.lang.Math.pow;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JFileChooser;
 
 import javax.swing.JOptionPane;
-
 /**
  *
  * @author Juampa Monroy
  */
 public class Ventana1 extends javax.swing.JFrame {
-
+    private File seleccion;
+    private Buscador buscar;
     /**
      * Creates new form Ventana1
      */
@@ -31,70 +36,180 @@ public class Ventana1 extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTree1 = new javax.swing.JTree();
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        nombreField = new javax.swing.JTextField();
+        seleccionCarpetaButton = new javax.swing.JButton();
+        buscarButton = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        extensionField = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        unidadCombo2 = new javax.swing.JComboBox<>();
+        unidadCombo1 = new javax.swing.JComboBox<>();
+        tamSpinner1 = new javax.swing.JSpinner();
+        tamSpinner2 = new javax.swing.JSpinner();
         jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        carpetaField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Buscador de Archivos");
 
         javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("Sin Resultados");
         jTree1.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
         jScrollPane1.setViewportView(jTree1);
 
-        jLabel1.setText("Criterio de Búsqueda:");
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Criterio de Búsqueda"));
 
-        jLabel2.setText("jLabel2");
+        jLabel1.setText("Nombre de Archivo:");
 
-        jButton1.setText("Seleccionar Carpeta");
+        seleccionCarpetaButton.setText("Seleccionar");
+        seleccionCarpetaButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                seleccionCarpetaButtonActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("Volver a Buscar");
+        buscarButton.setText("Buscar");
+        buscarButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buscarButtonActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Extensión:");
+
+        jLabel4.setText("Tamaño entre:");
+
+        jLabel5.setText("y");
+
+        unidadCombo2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KiB", "MiB", "GiB" }));
+
+        unidadCombo1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "KiB", "MiB", "GiB" }));
+
+        tamSpinner1.setModel(new javax.swing.SpinnerNumberModel(0.0d, 0.0d, null, 0.01d));
+        tamSpinner1.setToolTipText("");
+
+        tamSpinner2.setModel(new javax.swing.SpinnerNumberModel(0.0d, 0.0d, null, 0.01d));
+
+        jLabel2.setText("Carpeta:");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(carpetaField)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(seleccionCarpetaButton))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(nombreField))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(extensionField, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tamSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(buscarButton, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(unidadCombo1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel5)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(tamSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(unidadCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 250, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(seleccionCarpetaButton)
+                    .addComponent(carpetaField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(nombreField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(extensionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(26, 26, 26))
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel4)
+                        .addComponent(jLabel5)
+                        .addComponent(unidadCombo2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(tamSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(tamSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(unidadCombo1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(buscarButton))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(jLabel2)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(64, 64, 64)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 306, Short.MAX_VALUE)
-                .addContainerGap())
+                        .addComponent(jScrollPane1))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(10, 10, 10))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2)
-                        .addGap(8, 8, 8)
-                        .addComponent(jLabel2)))
-                .addContainerGap(29, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 318, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void seleccionCarpetaButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_seleccionCarpetaButtonActionPerformed
+        JFileChooser carpeta= new JFileChooser();
+        carpeta.setDialogType(JFileChooser.OPEN_DIALOG);
+        carpeta.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int opcion = carpeta.showOpenDialog(null);
+        if(opcion==JFileChooser.APPROVE_OPTION){
+            seleccion=carpeta.getSelectedFile();
+            if(seleccion.exists()&&seleccion.isDirectory())
+                carpetaField.setText(seleccion.toString());
+        }
+    }//GEN-LAST:event_seleccionCarpetaButtonActionPerformed
+
+    private void buscarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buscarButtonActionPerformed
+       seleccion=new File(carpetaField.getText());
+       if(seleccion.exists()){
+           String nombre=nombreField.getText().trim();
+           String extension= extensionField.getText().trim();
+           double menor=((double)(tamSpinner1.getValue()))*pow(1024,unidadCombo1.getSelectedIndex()+1);
+           double mayor=((double)(tamSpinner2.getValue()))*pow(1024,unidadCombo2.getSelectedIndex()+1);
+           buscar = new Buscador(seleccion,nombre,extension,menor, mayor);
+               
+       }
+    }//GEN-LAST:event_buscarButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -126,21 +241,28 @@ public class Ventana1 extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                for(int i=0;i<=1000;i++){
-                JOptionPane.showMessageDialog(null, "Juampa gaaay");
-                }
                 new Ventana1().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton buscarButton;
+    private javax.swing.JTextField carpetaField;
+    private javax.swing.JTextField extensionField;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTree jTree1;
+    private javax.swing.JTextField nombreField;
+    private javax.swing.JButton seleccionCarpetaButton;
+    private javax.swing.JSpinner tamSpinner1;
+    private javax.swing.JSpinner tamSpinner2;
+    private javax.swing.JComboBox<String> unidadCombo1;
+    private javax.swing.JComboBox<String> unidadCombo2;
     // End of variables declaration//GEN-END:variables
 }
